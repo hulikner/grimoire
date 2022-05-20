e8d18ac2b208298e34e2ca928d510b491a6c7b14
Hulikner <hulikner@gmail.com>
2022-05-20 17:01:03 -0500

    init

--- grimoire.js
+++ grimoire.js
@@ -1,89 +1,96 @@
 
 
-console.log("Do you believe in magic?");
-console.log("------------------------");
-
-List<Spell> allSpells = GetAllSpells();
-SpellBook goodBook = MakeGoodSpellBook(allSpells);
-SpellBook evilBook = MakeEvilSpellBook(allSpells);
-
-DisplaySpellBook(goodBook);
-console.log();
-DisplaySpellBook(evilBook);
 
 
-void DisplaySpellBook(SpellBook book)
+const GetAllSpells = () =>
 {
-    console.log(book.Title);
-    foreach (Spell aSpell in book.Spells)
-    {
-        console.log($"  {aSpell.Name}");
-    }
+    let allSpells =  [
+        {
+            Name: "Turn enemy into a newt",
+            IsEvil: true,
+            EnergyReqired: 5.1
+        },
+        {
+            Name: "Conjure some gold for a local charity",
+            IsEvil: false,
+            EnergyReqired: 2.99
+        },
+        {
+            Name: "Give a deaf person the ability to heal",
+            IsEvil: false,
+            EnergyReqired: 12.2
+        },
+        {
+            Name: "Make yourself emperor of the universe",
+            IsEvil: true,
+            EnergyReqired: 100.0
+        },
+        {
+            Name: "Convince your relatives your political views are correct",
+            IsEvil: false,
+            EnergyReqired: 2921.5
+        }
+    ];
+    
+    return allSpells;
 }
 
 
-SpellBook MakeEvilSpellBook(List<Spell> allSpells)
-{
-    SpellBook evilBook = new SpellBook();
-    evilBook.Title = "Evil Book";
-    evilBook.Spells = allSpells.Where(spell => spell.IsEvil).ToList();
+let DisplaySpellBook = (book) =>
+{   
+    // console.log(book)
+    book.forEach(book => console.log(`     ${book.Name}`))
+}
 
+
+const MakeEvilSpellBook = (allSpells) =>
+{
+    // SpellBook evilBook = SpellBook();
+    let evilBook= allSpells;
+    evilBook = evilBook.filter(spell => spell.IsEvil===true)
+    // evilBook.Spells = allSpells.Where(spell => spell.IsEvil).ToList();
+    // console.log(evilBook)
     return evilBook;
 }
 
 
-SpellBook MakeGoodSpellBook(List<Spell> allSpells)
+const MakeGoodSpellBook = (allSpells) =>
 {
-    SpellBook goodBook = new SpellBook();
-    goodBook.Title = "Good Book";
-    goodBook.Spells = allSpells.Where(spell => !spell.IsEvil).ToList();
-
+    let goodBook = allSpells;
+    // goodBook.Title = "Good Book";
+    goodBook = goodBook.filter(spell => spell.IsEvil===false)
+    // goodBook.Spells = allSpells.Where(spell => !spell.IsEvil).ToList();
+    
     return goodBook;
 }
 
+const allSpells = GetAllSpells();
+const goodBook = MakeGoodSpellBook(allSpells);
+const evilBook = MakeEvilSpellBook(allSpells);
+console.log("Do you believe in magic?");
+console.log("------------------------");
 
-List<Spell> GetAllSpells()
-{
-    List<Spell> allSpells = new List<Spell>() {
-        new Spell() {
-            Name = "Turn enemy into a newt",
-            IsEvil = true,
-            EnergyReqired = 5.1
-        },
-        new Spell() {
-            Name = "Conjure some gold for a local charity",
-            IsEvil = false,
-            EnergyReqired = 2.99
-        },
-        new Spell() {
-            Name = "Give a deaf person the ability to heal",
-            IsEvil = false,
-            EnergyReqired = 12.2
-        },
-        new Spell() {
-            Name = "Make yourself emperor of the universe",
-            IsEvil = true,
-            EnergyReqired = 100.0
-        },
-        new Spell() {
-            Name = "Convince your relatives your political views are correct",
-            IsEvil = false,
-            EnergyReqired = 2921.5
-        }
-    };
 
-    return allSpells;
-}
+console.log("Good Book")
+DisplaySpellBook(goodBook);
+console.log("Evil Book");
+DisplaySpellBook(evilBook);
 
-public class SpellBook
-{
-    public string Title { get; set; }
-    public List<Spell> Spells { get; set; }
-}
 
-public class Spell
-{
-    public string Name { get; set; }
-    public bool IsEvil { get; set; }
-    public double EnergyReqired { get; set; }
-}
+
+
+
+
+// public class SpellBook
+// {
+    //     public string Title { get; set; }
+    //     public List<Spell> Spells { get; set; }
+    // }
+    
+    // public class Spell
+    // {
+        //     public string Name { get; set; }
+        //     public bool IsEvil { get; set; }
+        //     public double EnergyReqired { get; set; }
+        // }
+        
